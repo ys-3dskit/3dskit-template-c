0981eccac710a251cb478beb24eeb84a156ccd67
Hazel Atkinson <yellowsink@riseup.net>
2025-01-07 22:30:27 +0000

    update toolchain

--- xmake.lua
+++ xmake.lua
@@ -6,25 +6,19 @@ add_requires(
 --, "citro2d ~1.6.0"
 )
 
-includes("toolchain/*.lua")
+includes("3dskit.lua")
 
 add_rules("mode.debug", "mode.release")
 
 target("your-app-name")
 	set_kind("binary")
-	set_plat("3ds")
 
-	set_arch("arm")
 	add_rules("3ds")
 	set_toolchains("devkitarm")
-	set_languages("c11")
 
 	set_values("3ds.name", "your-app-name")
 	set_values("3ds.description", "your app description")
 	set_values("3ds.author", "your author name")
-
-	-- TODO: this does not belong here. xmake won't play without it. -- sink
-	add_ldflags("-specs=3dsx.specs", "-g", "-march=armv6k", "-mtune=mpcore", "-mtp=soft", "-mfloat-abi=hard", {force = true})
 
 	add_files("src/**.c")
 
